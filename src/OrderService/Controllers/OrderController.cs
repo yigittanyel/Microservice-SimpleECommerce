1118e303bf9b2abf6dc9b02e33b87c5f71711ef7
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-10-06 16:12:24 +0300

    OrderController completed and ocelot.json file updated.

--- src/OrderService/Controllers/OrderController.cs
+++ src/OrderService/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+using OrderService.Services;
 
 namespace OrderService.Controllers
 {
@@ -7,5 +9,64 @@ namespace OrderService.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        // GET: api/<OrderController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var orders = await _orderService.GetOrdersAsync();
+            return Ok(orders);
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var order = await _orderService.GetOrderAsync(id);
+            if (order == null)
+                return NotFound();
+
+            return Ok(order);
+        }
+
+        // POST api/<OrderController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Order order)
+        {
+            var newOrder = await _orderService.CreateOrderAsync(order);
+            return CreatedAtAction(nameof(Get), new { id = newOrder.Id }, newOrder);
+        }
+
+        //DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _orderService.DeleteOrderAsync(id);
+            if (deleted == null)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        // PUT api/<OrderController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Order order)
+        {
+            if (id != order.Id)
+                return BadRequest();
+
+            var updatedOrder = await _orderService.UpdateOrderAsync(id, order);
+            if (updatedOrder == null)
+                return NotFound();
+
+            return Ok(updatedOrder);
+        }
+
     }
 }
