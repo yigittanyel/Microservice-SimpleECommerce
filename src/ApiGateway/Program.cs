432e3575226f66864047e795f617c81c8e9cee8b
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-10-06 14:32:46 +0300

    Ocelot configurations defined for api gateway layer.

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,15 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +20,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseOcelot().Wait();
 
 app.UseAuthorization();
 
